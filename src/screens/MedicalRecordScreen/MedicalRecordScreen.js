aa6c2fe31db8ec21ab0309f4a22d90ed991e0d5a
Filipe Góis <fythiy@gmail.com>
2024-02-11 20:57:01 -0300

    novo

--- src/screens/MedicalRecordScreen/MedicalRecordScreen.js
+++ src/screens/MedicalRecordScreen/MedicalRecordScreen.js
@@ -1,68 +1,53 @@
-
-import { Container, FormBox, MainContent, MainContentScroll } from "../../components/Container/style"
-import { BannerUserComponent } from "../../components/BannerUser"
-import UserImage from "../../assets/UserProfileImage.jpg"
-import { Button } from "../../components/Button/style"
-import { ButtonTitle } from "../../components/ButtonTitle/style"
-import Label from "../../components/Label"
-import { TextCreateAccount2 } from "../../components/Paragraph/style"
+import {
+  Container,
+  FormBox,
+  MainContent,
+  MainContentScroll,
+} from "../../components/Container/style";
+import { BannerUserComponent } from "../../components/BannerUser";
+import UserImage from "../../assets/UserProfileImage.jpg";
+import { Button } from "../../components/Button/style";
+import { ButtonTitle } from "../../components/ButtonTitle/style";
+import Label from "../../components/Label";
+import { TextCreateAccount2 } from "../../components/Paragraph/style";
 
 const DoctorHomeScreen = () => {
-    return (
-        <Container>
-            <MainContentScroll>
+  return (
+    <Container>
+      <MainContentScroll>
+        <MainContent>
+          <BannerUserComponent
+            src={UserImage}
+            name="Richard Kosta"
+            // isUser={false}
+            // specialty="Clínico geral"
+            // crm="123123"
+            age="22 anos"
+            email="richard.kosta@gmail.com"
+          />
 
-                <MainContent>
+          <FormBox>
+            <Label
+              titulo="Descrição da consulta"
+              placeholder={"Diagnóstico"}
+              fieldHeight={121}
+            />
+            <Label titulo="Diagnóstico do paciente" placeholder={"Descrição"} />
+            <Label
+              titulo="Prescrição médica"
+              placeholder={"Prescrição médica"}
+              fieldHeight={121}
+            />
 
-                    <BannerUserComponent
-                        src={UserImage}
-                        name="Richard Kosta"
-                        // isUser={false}
-                        // specialty="Clínico geral"
-                        // crm="123123"
-                        age="22 anos"
-                        email="richard.kosta@gmail.com"
+            <Button>
+              <ButtonTitle>Salvar</ButtonTitle>
+            </Button>
 
-                    />
-
-                    <FormBox>
-
-                        <Label
-                            titulo="Descrição da consulta"
-                            placeholder={"Diagnóstico"}
-                            fieldHeight={121}
-                        />
-                        <Label
-                            titulo="Diagnóstico do paciente"
-                            placeholder={"Descrição"}
-
-                        />
-                        <Label
-                            titulo="Prescrição médica"
-                            placeholder={"Prescrição médica"}
-                            fieldHeight={121}
-                        />
-
-
-                        <Button>
-                            <ButtonTitle>
-                                Salvar
-                            </ButtonTitle>
-                        </Button>
-
-                        <TextCreateAccount2>
-                            Cancelar
-                        </TextCreateAccount2>
-
-                    </FormBox>
-
-
-
-                </MainContent>
-
-
-            </MainContentScroll>
-        </Container>
-    )
-}
-export default DoctorHomeScreen
+            <TextCreateAccount2>Cancelar</TextCreateAccount2>
+          </FormBox>
+        </MainContent>
+      </MainContentScroll>
+    </Container>
+  );
+};
+export default DoctorHomeScreen;
