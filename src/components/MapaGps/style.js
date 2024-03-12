55a3600e3878a679391724eab08fa6a0f99d1ff7
Filipe Góis <fythoy@gmail.com>
2024-03-12 17:09:14 -0300

    Add MapViewDirections to show directions between two points on the map.

--- src/components/MapaGps/style.js
+++ src/components/MapaGps/style.js
@@ -4,6 +4,8 @@ import MapView from "react-native-maps";
 export const GpsBox = styled.View`
   width: 100%;
   height: 45%;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const MapaStyle = styled(MapView)`
